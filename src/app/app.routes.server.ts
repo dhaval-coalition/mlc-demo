f15f75fd622c7d81b09d805ce46493806879e656
dhaval-coalition <dhaval.mandaliya@coalitiontechnologies.com>
2025-08-26 16:22:25 +0530

    Add rendermode for all URLs

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,38 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: "",
+    renderMode: RenderMode.Server
+  },
+  {
+    path: "about",
+    renderMode: RenderMode.Server
+  },
+  {
+    path: "contact",
+    renderMode: RenderMode.Server
+  },
+  {
+    path: "loans",
+    renderMode: RenderMode.Server
+  },
+  {
+    path: "locations",
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'locations-detail',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: "blog",
+    renderMode: RenderMode.Server
+  },
+  {
+    path: "blog-detail",
+    renderMode: RenderMode.Server
+  },
+  {
     path: '**',
     renderMode: RenderMode.Server
   }
