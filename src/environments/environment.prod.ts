c6a8c108f31ea69571ec0aed3fcfefeef43e2f1d
dhaval-coalition <dhaval.mandaliya@coalitiontechnologies.com>
2025-08-29 20:46:58 +0530

    Add manually changes per support team

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://cdn.builder.io/api/v2/content/',
+  builderAPI: "9f51f6a9c8f847f0b5257ff975828bf8",
+  siteUrl: 'https://minuteloancenter.com',
+};
